cd703b8ba69b0c4da7a67f367b638285b7502fb0
Peter Treadaway <128326013+pedroespecial101@users.noreply.github.com>
2025-06-10 10:59:56 +0100

    Updated update offer.

--- start_mcp_inspector.sh
+++ start_mcp_inspector.sh
@@ -1,7 +1,7 @@
 
 print "Killing any existing FastMCP inspector processes..."
 lsof -ti :6277 | xargs kill -9
-lsof -ti :6274 | xarg-9s kill -9
+lsof -ti :6274 | xargs   kill -9
 
 echo "Starting FastMCP inspector..."
 npx @modelcontextprotocol/inspector python /Users/petetreadaway/Projects/ebay-mcp-server/src/main_server.py
